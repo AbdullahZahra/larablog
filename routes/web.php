5df7badddc9a18719a8fd93fd46b1c667fbd7fe7
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-09-03 09:15:38 +0200

    Add Posts with Form Request

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,4 +23,8 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('edit-category/{category_id}', [CategoryController::class, 'edit']);
     Route::put('update-category/{category_id}', [CategoryController::class, 'update']);
     Route::get('delete-category/{category_id}', [CategoryController::class, 'destroy']);
+
+    Route::get('posts', [PostController::class, 'index']);
+    Route::get('add-post', [PostController::class, 'create']);
+    Route::post('add-post', [PostController::class, 'store']);
 });
