30186f63f8a7aeb612aab7c8e0dacea66549c42e
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-09-06 23:56:10 +0200

    Owl Carousel Integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,12 @@
 
     <!-- Styles -->
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
+
+    {{-- Oul Carousel --}}
+    <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/owl.theme.default.min.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
@@ -29,7 +35,7 @@
 
         @include('layouts.inc.frontend-navbar')
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
@@ -37,6 +43,29 @@
     <!-- Scripts -->
     <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Oul Carousel -->
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script>
+        $('.category-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+    </script>
+
 </body>
 
 </html>
